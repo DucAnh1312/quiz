a66e1fcf9df01722531edca443b6265408a05c25
DucAnh1312 <109054934+DucAnh1312@users.noreply.github.com>
2022-10-07 19:56:27 +0700

    Delete src directory

--- src/config/Api.js
+++
@@ -1,4 +0,0 @@
-export const urlApi = "https://quangnh.xyz";
-export const loginApi = "https://quangnh.xyz/v1/authentication/login";
-export const registerApi = "https://quangnh.xyz/v1/authentication/register";
-export const forgotApi = "https://quangnh.xyz/v1/authentication/forgot-password";
--- src/config/regex.js
+++
@@ -1,2 +0,0 @@
-
-export const defaultThumbnail = 'https://res.cloudinary.com/qn052289/image/upload/v1664685443/gsriwi4r6ndzq5f5d1rz.webp'
